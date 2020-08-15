4104c2eb7f7e38e8f069bc0558983e347d7b4493
Nick Newman <nicknewman01@gmail.com>
2020-08-15 18:34:09 -0400

    can now handle multiple sensor outputs

--- Espresso_Arduino/Espresso_Arduino.ino
+++ Espresso_Arduino/Espresso_Arduino.ino
@@ -18,16 +18,27 @@
 */
 
 #include <ArduinoBLE.h>
+#include <Arduino_HTS221.h> // reads temperature and humidity
+#include <Arduino_LPS22HB.h> // reads the pressure
 
 const int ledPin = LED_BUILTIN; // set ledPin to on-board LED
 const int buttonPin = 4; // set buttonPin to digital pin 4
 
-BLEService ledService("19B10010-E8F2-537E-4F6C-D104768A1214"); // create service
+BLEService ledService("6a521c59-55b5-4384-85c0-6534e63fb09e"); // create service
 
 // create switch characteristic and allow remote device to read and write
-BLEByteCharacteristic ledCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEByteCharacteristic ledCharacteristic("6a521c60-55b5-4384-85c0-6534e63fb09e", BLERead | BLEWrite);
+
 // create button characteristic and allow remote device to get notifications
-BLEFloatCharacteristic buttonCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
+BLEFloatCharacteristic buttonCharacteristic("6a521c61-55b5-4384-85c0-6534e63fb09e", BLERead | BLENotify);
+
+// This will be used for the temperature sensor
+BLEFloatCharacteristic newCharacteristic("6a521c62-55b5-4384-85c0-6534e63fb09e", BLERead | BLENotify);
+
+void celsius_to_fahr(float &temp) {
+  temp = ((temp * 9) / 5) + 32;
+}
+
 
 void setup() {
   Serial.begin(9600);
@@ -43,6 +54,11 @@ void setup() {
     while (1);
   }
 
+  if (!HTS.begin()) {
+    Serial.println("Failed to initialize humidity/temperatute sensor!");
+    while (1);
+  }
+
   // set the local name peripheral advertises
   BLE.setLocalName("Nick's App");
   // set the UUID for the service this peripheral advertises:
@@ -51,12 +67,14 @@ void setup() {
   // add the characteristics to the service
   ledService.addCharacteristic(ledCharacteristic);
   ledService.addCharacteristic(buttonCharacteristic);
+  ledService.addCharacteristic(newCharacteristic);
 
   // add the service
   BLE.addService(ledService);
 
   ledCharacteristic.writeValue(0);
   buttonCharacteristic.writeValue(0);
+  newCharacteristic.writeValue(0);
 
   // start advertising
   BLE.advertise();
@@ -75,12 +93,21 @@ void loop() {
   // has the value changed since the last read
   boolean buttonChanged = (buttonCharacteristic.value() != buttonValue);
 
+  // Read the temperature and humidity values
+  float temperature = HTS.readTemperature();
+  float humidity = HTS.readHumidity();
+
+  celsius_to_fahr(temperature);
+
   if (buttonChanged) {
     // button state changed, update characteristics
     Serial.println("Button changed");
     Serial.println(buttonValue);
+    Serial.println("Temp value: ");
+    Serial.print(temperature);
     ledCharacteristic.writeValue(buttonValue);
     buttonCharacteristic.writeValue(buttonValue);
+    newCharacteristic.writeValue(temperature);
   }
   delay(3000);
 
